7080234d1ff08d5b7c734ac13a9f29031e3aa141
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-05-09 20:27:06 +0530

    the final update is done

--- hb.py
+++ hb.py
@@ -1,4 +1,61 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["python", "java", "ruby", "javascript", "csharp", "golang", "swift", "kotlin", "php", "typescript"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -7,6 +64,7 @@ word_length = len(chosen_word)
 for length in range(word_length):
     place_holders += "_"
 print(place_holders)
+lives = 6
 game_over = False
 correct_letters = []
 while not game_over:
@@ -23,10 +81,17 @@ while not game_over:
             display += "_"
                 
     print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Wrong guess! You have {lives} lives left.")
+        if lives == 0:
+            game_over = True
+            print("You lose!")
 
     if "_" not in display:
         game_over = True
         print("You win!")
+    print(stages[lives])
 
 
   
