bd7eee39e33a246b6de8d6f06a8ea586828e0ab0
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-05-15 21:53:36 +0530

    the calculator file is created

---
+++ calculator.py
@@ -0,0 +1,56 @@
+print("WELCOME TO THE PYTHON CALCULATOR BY BHUVANA!!!!")
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+def add(n1,n2):
+    return n1 + n2
+def subtract(n1,n2):
+    return n1 - n2
+def multiply(n1,n2):
+    return n1 * n2
+def divide(n1,n2):
+    return n1 / n2
+def modulus(n1,n2):
+    return n1 % n2
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "%": modulus,
+}
+def calculator():
+    num1 = float(input("What's the first number? "))
+    print("\n".join(operations.keys()))
+    should_continue = True
+    while should_continue == True:
+        operation_symbol = input("Pick an operation from the line above : ")
+        num2 = float(input("What's the next number? "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1,num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        user_input = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation : ")
+        if user_input  == "y":
+            num1 = answer
+        elif user_input == "n":
+            print("\n" * 100)
+            should_continue = False
+            calculator()
+
+
+calculator()
